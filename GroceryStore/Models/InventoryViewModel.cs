45276fcdcdf18e0f45cb6285d4b9edbd555421d4
Sai Phaninder Reddy Jonnala <jspreddy@yahoo.com>
2014-11-18 03:47:40 -0500

    create roles, seed users, assign roles. PART2

---
+++ GroceryStore/Models/InventoryViewModel.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+
+namespace GroceryStore.Models
+{
+    public class InventoryViewModel
+    {
+
+    }
+}
